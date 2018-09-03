3bb44c9e489b5baea404a3862f8e8d5e11249771
llillillill <42865406+llillillill@users.noreply.github.com>
2018-09-04 00:29:48 +0600

    Add files via upload

--- edit_db.php
+++ edit_db.php
@@ -7,6 +7,7 @@
     $b_id=$_SESSION['b_id'];
 
     echo "aise";
+    //for restoration
     if(isset($_POST['restore'])){
         $sql="UPDATE book SET is_deleted = '0' WHERE book.b_id = '$b_id' ";
         $conn->query($sql);
@@ -15,20 +16,13 @@
         $_SESSION['retore_success']="restored!";
     }
     else if(isset($_POST['edit'])){
-        $sql="UPDATE book SET is_deleted = '0' WHERE book.b_id = '$b_id' ";
-        $conn->query($sql);
-        echo "restored!";
-        unset($_SESSION[$b_id]);
-        unset($_POST['restore']);
-        $_SESSION['retore_success']="restored!";
-        echo "<script> location.href='editDb.php'; </script>"; 
-        exit;
+        
     }
     else if(isset($_POST['delete'])){
         $sql="UPDATE book SET is_deleted = '1' WHERE book.b_id = '$b_id' ";
         $conn->query($sql);
         unset($_SESSION[$b_id]);
-        unset($_POST['delete_success']);
+        unset($_POST['delete']);
         $_SESSION['delete_success']="delted!";
     }
 
